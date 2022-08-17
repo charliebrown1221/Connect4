354a36bae64326683c598bd8ae9bd82783b3c215
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-17 11:43:40 -0400

    <feat> created functions drop_piece,is_valid_location,get_next_open_row and added fumciality to player 1&2

--- connect4.py
+++ connect4.py
@@ -1,9 +1,21 @@
 import numpy as np
 
+ROW_COUNT = 6
+COLUMN_COUNT= 7
+
 def  create_board():
     board =np.zeros((6,7))
     return board
+def drop_piece(board,row,col,piece):
+    board[row][col]==piece
 
+def is_valid_location(board,col):
+    return board[5][col]==0
+
+def get_next_open_row(board,col):
+    for r in range(ROW_COUNT):
+        if board[r][col]==0:
+            return r
 
 board = create_board()
 game_over=False
@@ -15,12 +27,19 @@ turn=0
 
 while not game_over:
     if turn==0:
-        selection = int(input("Player 1 Make your selection(0-6): "))
+        col = int(input("Player 1 Make your selection(0-6): "))
+        if is_valid_location(board,col):
+            row = get_next_open_row(board,col)
+            drop_piece(board,col,row,1)
+
 
 
 
     else:
-        selection = int(input("Player 2 Make your selection(0-6): "))
+        col = int(input("Player 2 Make your selection(0-6): "))
+        if is_valid_location(board,col):
+            row = get_next_open_row(board,col)
+            drop_piece(board,col,row,2)
 
     turn +=1
     turn = turn % 2
