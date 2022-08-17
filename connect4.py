804f6bfe94c52b7ba912039c96692e049e9806ab
charliebrown1221 <chaimbrown1@yahoo.com>
2022-08-17 11:28:41 -0400

    <feat> created turns

--- connect4.py
+++ connect4.py
@@ -15,4 +15,18 @@ turn=0
 
 while not game_over:
     if turn==0:
-        selection =input("Player one Make your selection")
+        selection = int(input("Player 1 Make your selection(0-6): "))
+
+
+
+    else:
+        selection = int(input("Player 2 Make your selection(0-6): "))
+
+    turn +=1
+    turn = turn % 2
+
+
+
+   
+
+        
